77cdc79f7301c6af08cc168a549473b0edb3bb3e
Bruna Ribeiro <brunaribeiro.ufv@gmail.com>
2025-04-26 18:25:47 -0300

    create project

---
+++ src/main/java/com/brunaribeiro/meeting_room/MeetingRoomApplication.java
@@ -0,0 +1,13 @@
+package com.brunaribeiro.meeting_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomApplication.class, args);
+	}
+
+}
